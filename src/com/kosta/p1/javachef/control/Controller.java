0b9116d06d45dec01c2e36bdd0ffb71bb29ec1cd
seul <hsg0926@gmail.com>
2017-03-29 22:38:18 +0900

    황슬기 - money삭제 total추가 model수정 controller수정

--- src/com/kosta/p1/javachef/control/Controller.java
+++ src/com/kosta/p1/javachef/control/Controller.java
@@ -9,16 +9,15 @@ import java.util.Vector;
 
 import com.kosta.p1.javachef.model.Item;
 import com.kosta.p1.javachef.model.Model;
-import com.kosta.p1.javachef.model.Money;
 import com.kosta.p1.javachef.view.AdminView;
 import com.kosta.p1.javachef.view.MainView;
 
-public class Controller  extends Thread implements ActionListener {
+public class Controller implements ActionListener {
 	MainView m_View;
 	AdminView ad_View;
 	
 	Model m;
-	Vector<String> iv, mv;//iv:메뉴 잠시 저장하는 벡터, mv:돈 잠시 저장하는 벡터
+	Vector<String> iv;//iv:메뉴 잠시 저장하는 벡터, mv:돈 잠시 저장하는 벡터
 
 	public Controller() {
 		m_View = new MainView();
@@ -26,25 +25,16 @@ public class Controller  extends Thread implements ActionListener {
 		
 		m = new Model();
 		iv = new Vector<>();
-		mv = new Vector<>();
 		
-		start();
-		
-		this.eventUp();
-	}
-	
-	public void run(){
 		try {
 	         BufferedReader ibr = new BufferedReader(new FileReader("data/item.txt"));//메뉴들의 정보 파일을 가져온다.
-	         //FileWriter fw = new FileWriter("item.txt");
+	         //FileWriter ifw = new FileWriter("data/item.txt");
 	         
 	         do{
 	            String iread = ibr.readLine();//메뉴 파일을 한줄씩 읽는다
 	            if(iread==null) break;//값이 없으면 더이상 읽지 않는다
 	            
-	            String iread2[] = iread.split("=");//읽으면 =을 기준으로 값을 나눈다
-	            
-	            iv.add(iread2[1]);//인덱스1을 itemV벡터에 차례대로 넣는다
+	            iv.add(iread);//인덱스1을 itemV벡터에 차례대로 넣는다
 
 	            if(iv.size()==4){//itemV의 크기가 4가 되면
 	            	String itemName = iv.get(0);
@@ -61,46 +51,20 @@ public class Controller  extends Thread implements ActionListener {
 	            
 	         }while(true);
 	         
-	         this.itemView(); //자판기 상품가격 표시	         
-	            
-	         BufferedReader mbr = new BufferedReader(new FileReader("data/money.txt"));//돈의 남은 수들(돈의 정보)을 가져온다.
-
-	         do{
-	            String mread = mbr.readLine();//돈 파일을 한줄씩 읽는다
-	            if(mread==null) break;//값이 없으면 더이상 읽지 않는다
-	            
-	            String mread2[] = mread.split("=");//읽으면 =을 기준으로 값을 나눈다
-	            
-	            mv.add(mread2[1]);//인덱스1을 itemV벡터에 차례대로 넣는다
-
-	            if(mv.size()==4){//itemV의 크기가 4가 되면
-	            	int manwon_num = Integer.parseInt(mv.get(0));
-	            	int ohchunwon_num = Integer.parseInt(mv.get(1));
-	            	int chunwon_num = Integer.parseInt(mv.get(2));
-	            	int ohbaekwon_num = Integer.parseInt(mv.get(3));
-	            	//int 
-	            	
-	            	Money money = new Money(manwon_num, ohchunwon_num, chunwon_num, ohbaekwon_num);
-	            	
-	            	m.insert(money);
-	               
-	            	mv.removeAllElements();//하나의 메뉴 itemV가 생성되면 모두 지워 다른 메뉴를 넣을 준비를 한다.
-	            }
-	            
-	         }while(true);
-	         
-	         Vector<Money> iii = m.selectmoneyAll();
-	            for(int i=0; i<iii.size(); i++){
-	    			Money p = iii.get(i);
-//	    			System.out.print(p.getManwon_num()+" ");
-//	    			System.out.print(p.getOhchunwon_num()+" ");
-//	    			System.out.print(p.getChunwon_num()+" ");
-//	    			System.out.print(p.getOhbaekwon_num()+"\n");
+	         Vector<Item> ii = m.selectitemAll();
+	            for(int i=0; i<ii.size(); i++){
+	    			Item p = ii.get(i);
+	    			System.out.print(p.getItemName()+" ");
+	    			System.out.print(p.getItemPrice()+" ");
+	    			System.out.print(p.getItemNum()+" ");
+	    			System.out.print(p.getItemAcc()+"\n");
 	    		}
 
 	      } catch (IOException e) {
 	         e.printStackTrace();
 	      }
+		
+		this.eventUp();
 	}
 	public void remainderItems(){//재고수량을 관리자 모드에서 표시
 		ad_View.ta_inven1.setText(""+m.itemV.get(0).getItemNum());
--- src/com/kosta/p1/javachef/model/Model.java
+++ src/com/kosta/p1/javachef/model/Model.java
@@ -2,13 +2,13 @@ package com.kosta.p1.javachef.model;
 
 import java.util.Vector;
 
-public class Model {
-	public Vector<Item> itemV;
-	Vector<Money> moneyV;
-
+public class Model{
+	Vector<Item> itemV;
+	Item it;
+	
 	public Model() {
 		itemV = new Vector<>();
-		moneyV = new Vector<>();
+
 	}
 
 	public void insert(Item item) {
@@ -17,13 +17,33 @@ public class Model {
 
 	public Vector selectitemAll() {
 		return itemV;
+	}	
+	
+	public void updateItem(String itemName, int itemAcc, int total){
+		
+		for(int i=0;i<itemV.size();i++){
+			
+			it=itemV.get(i);
+			     
+			  if(itemName.equals(it.getItemName())){
+				  
+				  
+				  it.setItemAcc(it.getItemAcc()+itemAcc);             
+				  it.setItemNum(it.getItemNum()-itemAcc);
+				  
+				  //start();
+				  
+				  
+			  }//if
+			
+			
+		}//for
+		
+		
+	}//updateItem
+	
+	public static void main(String[] args) {
+		new Model();
 	}
-
-	public void insert(Money money) {
-		moneyV.add(money);
-	}
-
-	public Vector selectmoneyAll() {
-		return moneyV;
-	}
+	
 }
