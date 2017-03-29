39d9d311940fce2c426769bd0acca6dc73e32aa1
Rustiness <hi0001009@gmail.com>
2017-03-29 15:54:41 +0900

    김종기 -슬기씨 수정부분 추가

--- src/com/kosta/p1/javachef/model/Item.java
+++ src/com/kosta/p1/javachef/model/Item.java
@@ -10,6 +10,13 @@ public class Item {// ¿⁄πŸ∫Û¡Ó
 
 	}
 
+	public Item(String itemName, int itemPrice, int itemNum, int itemAcc) {
+		this.itemName = itemName;
+		this.itemPrice = itemPrice;
+		this.itemNum = itemNum;
+		this.itemAcc = itemAcc;
+	}
+
 	public String getItemName() {
 		return itemName;
 	}
