66b4912f081a566d74e76b345034e839c2562516
Rustiness <hi0001009@gmail.com>
2017-03-29 12:09:28 +0900

    황슬기 -진행작업 업데이트

--- src/com/kosta/p1/javachef/model/Item.java
+++ src/com/kosta/p1/javachef/model/Item.java
@@ -1,55 +1,5 @@
 package com.kosta.p1.javachef.model;
 
-public class Item {// 자바빈즈
-	private String itemName; // 상품명
-	private int itemPrice; // 판매가
-	private int itemNum; // 수량
-	private int itemAcc; // 누적판매수량
+public class Item { //자바빈즈
 
-	public Item() {
-
-   }
-   
-   public Item(String itemName, int itemPrice, int itemNum, int itemAcc){
-	   this.itemName = itemName;
-	   this.itemPrice = itemPrice;
-	   this.itemNum = itemNum;
-	   this.itemAcc = itemAcc;
-   }
-
-	public String getItemName() {
-		return itemName;
-	}
-
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
-
-	public int getItemPrice() {
-		return itemPrice;
-	}
-
-	public void setItemPrice(int itemPrice) {
-		this.itemPrice = itemPrice;
-	}
-
-	public int getItemNum() {
-		return itemNum;
-	}
-
-	public void setItemNum(int itemNum) {
-		this.itemNum = itemNum;
-	}
-
-	public int getItemAcc() {
-		return itemAcc;
-	}
-
-	public void setItemAcc(int itemAcc) {
-		this.itemAcc = itemAcc;
-	}
-
-	public static void main(String[] args) {
-		new Item();
-	}
 }
--- src/com/kosta/p1/javachef/model/Model.java
+++ src/com/kosta/p1/javachef/model/Model.java
@@ -1,28 +1,5 @@
 package com.kosta.p1.javachef.model;
 
-import java.util.Vector;
-
 public class Model {
-	Vector<Item> itemV;
-	Vector<Money> moneyV;
 
-	public Model() {
-		itemV = new Vector<>();
-		moneyV = new Vector<>();
-
-	}
-   public void insert(Item item){
-	   itemV.add(item);
-   }
-	   public Vector selectitemAll(){
-		return itemV;
-   }
-   
-   public void insert(Money money){
-	   moneyV.add(money);
-   }
-   
-   public Vector selectmoneyAll(){
-		return moneyV;
-  }
 }
