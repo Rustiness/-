679322d597226a8dc6c4a5f0795fee97cdb19e1a
minhaskim <minhaskim@gmail.com>
2017-03-30 11:04:39 +0900

    민하 Admin_View: bt_add1~6 and minus1~6 추가_ scroll_bar 1~6 추가 scroll_bar 배열로 처리 기존 bt_add1~6을 ta_add1~6로 변경

--- src/com/kosta/p1/javachef/view/AdminView.java
+++ src/com/kosta/p1/javachef/view/AdminView.java
@@ -1,5 +1,8 @@
 package com.kosta.p1.javachef.view;
 
+import java.awt.Color;
+import java.awt.Scrollbar;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,11 +14,16 @@ import javax.swing.JTextArea;
 public class AdminView extends JFrame {
 
 	public JButton bt_mainView, bt_return;
-	public JButton bt_add1, bt_add2, bt_add3, bt_add4, bt_add5, bt_add6; 
-	public JTextArea  ta_inven1, ta_inven2, ta_inven3, ta_inven4, ta_inven5, ta_inven6;
-		   JLabel la_top,la_sale1, la_sale2, la_sale3, la_sale4, la_sale5, la_sale6, la_saletotal, la_ready, la_total;
+	public JTextArea ta_add1, ta_add2, ta_add3, ta_add4, ta_add5, ta_add6;//제품명,제품가격,현재수량 
+	public JTextArea ta_add_arr[] = new JTextArea[6];
+	
+	public JTextArea  ta_inven1, ta_inven2, ta_inven3, ta_inven4, ta_inven5, ta_inven6;//재고수량
+	public JTextArea ta_inven_arr[] = new JTextArea[6];
+	
+	public JLabel la_top,la_sale1, la_sale2, la_sale3, la_sale4, la_sale5, la_sale6, la_saletotal, la_ready, la_total;
 	public JTextArea ta_item;
-//	public JScrollBar scroll_inven1, scroll_inven2, scroll_inven3, scroll_inven4, scroll_inven5, scroll_inven6;
+	public JScrollBar scroll_inven1, scroll_inven2, scroll_inven3, scroll_inven4, scroll_inven5, scroll_inven6;
+	public JButton bt_add1, bt_minus1,bt_add2, bt_minus2, bt_add3, bt_minus3, bt_add4, bt_minus4, bt_add5, bt_minus5, bt_add6, bt_minus6;
 	ImageIcon bt1;
 
 	public AdminView() {
@@ -26,19 +34,48 @@ public class AdminView extends JFrame {
 		bt_mainView.setBounds(500, 20, 80, 30);
 
 		bt1 = new ImageIcon("don.png");// 사진크기는 1.8cm(정사각형)
-		bt_add1 = new JButton(bt1);
-		bt_add1.setBounds(40, 100, 100, 100);
-		bt_add2 = new JButton("아이템2");
-		bt_add2.setBounds(190, 100, 100, 100);
-		bt_add3 = new JButton("아이템3");
-		bt_add3.setBounds(40, 300, 100, 100);
-		bt_add4 = new JButton("아이템4");
-		bt_add4.setBounds(190, 300, 100, 100);
-		bt_add5 = new JButton("아이템5");
-		bt_add5.setBounds(40, 500, 100, 100);
-		bt_add6 = new JButton("아이템6");
-		bt_add6.setBounds(190, 500, 100, 100);
-
+		ta_add1 = new JTextArea();
+		ta_add1.setBounds(40, 100, 100, 100);
+		ta_add1.setBackground(Color.ORANGE );
+		ta_add2 = new JTextArea();
+		ta_add2.setBounds(190, 100, 100, 100);
+		ta_add2.setBackground(Color.ORANGE );
+		ta_add3 = new JTextArea();
+		ta_add3.setBounds(40, 300, 100, 100);
+		ta_add3.setBackground(Color.ORANGE );
+		ta_add4 = new JTextArea();
+		ta_add4.setBounds(190, 300, 100, 100);
+		ta_add4.setBackground(Color.ORANGE );
+		ta_add5 = new JTextArea();
+		ta_add5.setBounds(40, 500, 100, 100);
+		ta_add5.setBackground(Color.ORANGE );
+		ta_add6 = new JTextArea();
+		ta_add6.setBounds(190, 500, 100, 100);
+		ta_add6.setBackground(Color.ORANGE );
+        
+				//Addition
+				ta_add_arr[0] = ta_add1;
+				ta_add_arr[1] = ta_add2;
+				ta_add_arr[2] = ta_add3;
+				ta_add_arr[3] = ta_add4;
+				ta_add_arr[4] = ta_add5;
+				ta_add_arr[5] = ta_add6;
+				
+		
+		bt_add1 = new JButton("추가");
+		bt_minus1 = new JButton("회수");
+		bt_add2 = new JButton("추가");
+		bt_minus2 = new JButton("회수");
+		bt_add3 = new JButton("추가");
+		bt_minus3 = new JButton("회수");
+		bt_add4 = new JButton("추가");
+		bt_minus4 = new JButton("회수");
+		bt_add5 = new JButton("추가");
+		bt_minus5 = new JButton("회수");
+		bt_add6 = new JButton("추가");
+		bt_minus6 = new JButton("회수");
+		
+				
 		ta_item = new JTextArea();
 		ta_item.setEditable(false);
 		ta_item.setBounds(340, 100, 200, 250);
@@ -55,7 +92,27 @@ public class AdminView extends JFrame {
 		ta_inven5.setBounds(42, 600, 40, 40);
 		ta_inven6 = new JTextArea();
 		ta_inven6.setBounds(192, 600, 40, 40);
+		
+			//Addition
+			ta_inven_arr[0] = ta_inven1;
+			ta_inven_arr[1] = ta_inven2;
+			ta_inven_arr[2] = ta_inven3;
+			ta_inven_arr[3] = ta_inven4;
+			ta_inven_arr[4] = ta_inven5;
+			ta_inven_arr[5] = ta_inven6;
 
+		scroll_inven1 = new JScrollBar(Scrollbar.HORIZONTAL, 0, 2, 0, 10);
+		scroll_inven2 = new JScrollBar(Scrollbar.HORIZONTAL, 0, 2, 0, 10);
+		scroll_inven3 = new JScrollBar(Scrollbar.HORIZONTAL, 0, 2, 0, 10);
+		scroll_inven4 = new JScrollBar(Scrollbar.HORIZONTAL, 0, 2, 0, 10);
+		scroll_inven5 = new JScrollBar(Scrollbar.HORIZONTAL, 0, 2, 0, 10);
+		scroll_inven6 = new JScrollBar(Scrollbar.HORIZONTAL, 0, 2, 0, 10);
+		scroll_inven1.setBounds(40,250,100,20);
+		scroll_inven2.setBounds(190,250,100,20);
+		scroll_inven3.setBounds(40,450,100,20);
+		scroll_inven4.setBounds(190,450,100,20);
+		scroll_inven5.setBounds(40,650,100,20);
+		scroll_inven6.setBounds(190,650,100,20);
       /*
        la_sale1=new JLabel("아이템1 판매량: ");      
        la_sale2=new JLabel("아이템2 판매량: ");      
@@ -75,12 +132,12 @@ public class AdminView extends JFrame {
 
 		add(la_top);
 		add(bt_mainView);
-		add(bt_add1);
-		add(bt_add2);
-		add(bt_add3);
-		add(bt_add4);
-		add(bt_add5);
-		add(bt_add6);
+		add(ta_add1);
+		add(ta_add2);
+		add(ta_add3);
+		add(ta_add4);
+		add(ta_add5);
+		add(ta_add6);
 		add(ta_inven1);
 		add(ta_inven2);
 		add(ta_inven3);
@@ -91,6 +148,25 @@ public class AdminView extends JFrame {
 		add(la_ready);
 		add(la_total);
 		add(bt_return);
+		add(scroll_inven1);
+		add(scroll_inven2);
+		add(scroll_inven3);
+		add(scroll_inven4);
+		add(scroll_inven5);
+		add(scroll_inven6);
+		add(bt_add1);
+		add(bt_add2);
+		add(bt_add3);
+		add(bt_add4);
+		add(bt_add5);
+		add(bt_add6);
+		add(bt_minus1);
+		add(bt_minus2);
+		add(bt_minus3);
+		add(bt_minus4);
+		add(bt_minus5);
+		add(bt_minus6);
+		
 
 		this.setTitle("자판기(관리자)");
 		this.setLayout(null);
